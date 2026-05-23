e7edf02170cd0686b9b202aa20e600d207cc1eeb
pakize <pduran@web.de>
2026-05-23 16:54:36 +0200

    add collisionShape depending on movement

--- Scripts/Game/player.gd
+++ Scripts/Game/player.gd
@@ -1,10 +1,24 @@
 extends CharacterBody2D
 
+@onready var hitbox = $LegCollisionShape
+
+
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("moveLeft"):
 			$AnimatedSprite2D.frame = 0
 	if Input.is_action_just_pressed("moveRight"):
 			$AnimatedSprite2D.frame = 2
+	match $AnimatedSprite2D.frame:
+		0:
+			hitbox.position = Vector2(662, 492)  # Bein links
+			hitbox.shape.size = Vector2(86, 165)
+			
+		1:
+			hitbox.position = Vector2(547, 492)   # Stand
+			hitbox.shape.size = Vector2(750, 165)
+		2:
+			hitbox.position = Vector2(547, 488.5) # Bein rechts
+			hitbox.shape.size = Vector2(86, 165)			
 
 	pass	
